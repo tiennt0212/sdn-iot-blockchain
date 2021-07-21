06987820e760dcb4378cfe301d3d3ab02f5d3046
ThanhTien <tiennt0212@gmail.com>
2021-07-21 05:22:16 -0700

    Transaction Processor working directory

---
+++ oceansong-tp/pyclient/client/ocean_cli.py
@@ -0,0 +1,145 @@
+import argparse
+import getpass
+import logging
+import os
+import sys
+import traceback
+import pkg_resources
+
+from colorlog import ColoredFormatter
+
+from client.ocean_client import OceanClient
+
+DISTRIBUTION_NAME = 'simplewallet'
+
+DEFAULT_URL = 'http://localhost:8008'
+
+def create_console_handler(verbose_level):
+    clog = logging.StreamHandler()
+    formatter = ColoredFormatter(
+        "%(log_color)s[%(asctime)s %(levelname)-8s%(module)s]%(reset)s "
+        "%(white)s%(message)s",
+        datefmt="%H:%M:%S",
+        reset=True,
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red',
+        })
+
+    clog.setFormatter(formatter)
+    clog.setLevel(logging.DEBUG)
+    return clog
+
+def setup_loggers(verbose_level):
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(create_console_handler(verbose_level))
+
+def add_register_parser(subparsers, parent_parser):
+    '''Define the "register" command line parsing.'''
+    parser = subparsers.add_parser(
+        'register',
+        help='register new IoT device',
+        parents=[parent_parser])
+
+    parser.add_argument(
+        'deviceID',
+        type=int,
+        help='the ID of IoT device to register')
+
+    parser.add_argument(
+        'gatewayKey',
+        type=str,
+        help='the Key of gateway of this device')
+
+def create_parent_parser(prog_name):
+    '''Define the -V/--version command line options.'''
+    parent_parser = argparse.ArgumentParser(prog=prog_name, add_help=False)
+
+    try:
+        version = pkg_resources.get_distribution(DISTRIBUTION_NAME).version
+    except pkg_resources.DistributionNotFound:
+        version = 'UNKNOWN'
+
+    parent_parser.add_argument(
+        '-V', '--version',
+        action='version',
+        version=(DISTRIBUTION_NAME + ' (Hyperledger Sawtooth) version {}')
+        .format(version),
+        help='display version information')
+
+    return parent_parser
+
+
+def create_parser(prog_name):
+    '''Define the command line parsing for all the options and subcommands.'''
+    parent_parser = create_parent_parser(prog_name)
+
+    parser = argparse.ArgumentParser(
+        description='Provides subcommands to manage your simple wallet',
+        parents=[parent_parser])
+
+    subparsers = parser.add_subparsers(title='subcommands', dest='command')
+
+    subparsers.required = True
+
+    add_register_parser(subparsers, parent_parser)
+
+    return parser
+
+def _get_keyfile(customerName):
+    '''Get the private key for a customer.'''
+    home = os.path.expanduser("~")
+    key_dir = os.path.join(home, ".sawtooth", "keys")
+
+    return '{}/{}.priv'.format(key_dir, customerName)
+
+def _get_pubkeyfile(customerName):
+    '''Get the public key for a customer.'''
+    home = os.path.expanduser("~")
+    key_dir = os.path.join(home, ".sawtooth", "keys")
+
+    return '{}/{}.pub'.format(key_dir, customerName)
+
+def do_register(args):
+    '''Implements the "register" subcommand by calling the client class.'''
+    keyfile = _get_keyfile(args.gatewayKey)
+
+    client = OceanClient(baseUrl=DEFAULT_URL, keyFile=keyfile)
+
+    response = client.register(args.deviceID)
+
+    print("Response: {}".format(response))
+
+def main(prog_name=os.path.basename(sys.argv[0]), args=None):
+    '''Entry point function for the client CLI.'''
+    if args is None:
+        args = sys.argv[1:]
+    parser = create_parser(prog_name)
+    args = parser.parse_args(args)
+
+    verbose_level = 0
+
+    setup_loggers(verbose_level=verbose_level)
+
+    # Get the commands from cli args and call corresponding handlers
+    if args.command == 'register':
+        do_register(args)
+    else:
+        raise Exception("Invalid command: {}".format(args.command))
+
+
+def main_wrapper():
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    except SystemExit as err:
+        raise err
+    except BaseException as err:
+        traceback.print_exc(file=sys.stderr)
+        sys.exit(1)
+
---
+++ oceansong-tp/pyprocessor/processor/ocean_tp.py
@@ -0,0 +1,136 @@
+import traceback
+import sys
+import hashlib
+import logging
+import tokenlib
+
+from sawtooth_sdk.processor.handler import TransactionHandler
+from sawtooth_sdk.processor.exceptions import InvalidTransaction
+from sawtooth_sdk.processor.exceptions import InternalError
+from sawtooth_sdk.processor.core import TransactionProcessor
+
+LOGGER = logging.getLogger(__name__)
+
+FAMILY_NAME = "oceansong"
+
+def _hash(data):
+    '''Compute the SHA-512 hash and return the result as hex characters.'''
+    return hashlib.sha512(data).hexdigest()
+
+def _generate_token(id):
+    manager = tokenlib.TokenManager(secret="OCEANSONG")
+    token = manager.make_token(
+        {
+            "deviceId": id,
+        })
+    return token;
+# Prefix for simplewallet is the first six hex digits of SHA-512(TF name).
+sw_namespace = _hash(FAMILY_NAME.encode('utf-8'))[0:6]
+
+class SimpleWalletTransactionHandler(TransactionHandler):
+    '''                                                       
+    Transaction Processor class for the OCEANSONG transaction family.       
+                                                              
+    This with the validator using the accept/get/set functions.
+    It implements functions to REGISTER and APPLY rules for any devices.
+    '''
+
+    def __init__(self, namespace_prefix):
+        self._namespace_prefix = namespace_prefix
+
+    @property
+    def family_name(self):
+        return FAMILY_NAME
+
+    @property
+    def family_versions(self):
+        return ['1.0']
+
+    @property
+    def namespaces(self):
+        return [self._namespace_prefix]
+
+    def apply(self, transaction, context):
+        '''This implements the apply function for this transaction handler.
+                                                              
+           This function does most of the work for this class by processing
+           a single transaction for the OCEANSONG transaction family.   
+        '''                                                   
+        # Unpack transaction
+        # Get the payload and extract information.
+        header = transaction.header
+        payload_list = transaction.payload.decode().split(",")
+        command = payload_list[0]
+        id = payload_list[1]
+
+        # Get the public key sent from the client.
+        from_key = header.signer_public_key
+
+        # Perform the command.
+        LOGGER.info("Command = "+ command)
+
+        if command == "register":
+            self._register_token(context, id, from_key)
+        else:
+            LOGGER.info("Unhandled action. " +
+                "Command should be register or transfer data")
+
+    def _register_token(self, context, id, from_key):
+        gateway_address = self._get_gateway_address(from_key)
+        LOGGER.info('Got the key {} and the gateway address {} '.format(
+            from_key, gateway_address))
+        current_entry = context.get_state([gateway_address])
+        newtoken = _generate_token(id)
+        if current_entry == []:
+            LOGGER.info('No previous Gateway, creating new Gateway {} '
+                .format(from_key))
+            #Make a new token list
+            tokenList = []
+            tokenList.append(newtoken)
+            # newtoken = int(id)rejected due to state root hash mismatch
+        else:
+            tokenList = current_entry[0].data.decode('utf-8')
+            tokenList.append(newtoken)
+            # oldtoken = int(current_entry[0].data)
+            # newtoken = oldtoken + int(id)
+
+        #Add a new token to tokenList
+        # tokenList.append(token)
+
+        #Store data to BlockChain
+        state_data = str(newtoken).encode('utf-8')
+        addresses = context.set_state({gateway_address: state_data})
+
+        if len(addresses) < 1:
+            raise InternalError("State Error")
+
+    def _get_gateway_address(self, from_key):
+        return _hash(FAMILY_NAME.encode('utf-8'))[0:6] + _hash(from_key.encode('utf-8'))[0:64]
+
+
+
+def setup_loggers():
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+
+def main():
+    '''Entry-point function for the simplewallet transaction processor.'''
+    setup_loggers()
+    try:
+        # Register the transaction handler and start it.
+        processor = TransactionProcessor(url='tcp://localhost:4004')
+
+        handler = SimpleWalletTransactionHandler(sw_namespace)
+
+        processor.add_handler(handler)
+
+        processor.start()
+
+    except KeyboardInterrupt:
+        pass
+    except SystemExit as err:
+        raise err
+    except BaseException as err:
+        traceback.print_exc(file=sys.stderr)
+        sys.exit(1)
+
